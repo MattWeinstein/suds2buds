deec5db593a42bb51923b436a7da423c2d6bf182
MattWeinstein <102390205+MattWeinstein@users.noreply.github.com>
2023-03-21 21:12:40 -0400

    removed main.js

--- public/main.js
+++ public/main.js
@@ -1,18 +1,18 @@
-document.querySelector('#deleteButton').addEventListener('click',deleteEverything)
+// document.querySelector('#deleteButton').addEventListener('click',deleteEverything)
 
-async function deleteEverything(){
-    try{
-    const response = await fetch('/beers',{
-        method:'delete',
-        headers:{'content-type':'application/json'}
-        })
-        const data = await response.json()
-        location.reload()
-    }catch(err){
-        console.log(err)
-    }
-}
+// async function deleteEverything(){
+//     try{
+//     const response = await fetch('/beers',{
+//         method:'delete',
+//         headers:{'content-type':'application/json'}
+//         })
+//         const data = await response.json()
+//         location.reload()
+//     }catch(err){
+//         console.log(err)
+//     }
+// }
 
    
-// Get the inner text of the paragraph with email
-// if that doesn't exist, you need an email ot be entered
+// // Get the inner text of the paragraph with email
+// // if that doesn't exist, you need an email ot be entered
